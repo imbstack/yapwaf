b469c6554be3a15665c676c75f7b2cdc4ea0c7fd
Brian Stack <bis12@case.edu>
2012-11-16 18:51:04 -0500

    Initial packaget setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import yapwaf
+
+setup(
+  name = 'yapwaf',
+  version = yapwaf.__version__,
+  description = 'Simple lightweight WSGI application framework',
+  author = 'Brian Stack',
+  author_email = 'bis12@case.edu',
+  url = 'http://github.com/bis12/yapwaf',
+  download_url = \
+    'http://github.com/bis12/yapwaf/tarball/yapwaf-%s' % yapwaf.__version__,
+  packages = find_packages('yapwaf', exclude=['ez_setup']),
+  provides = ['yapwaf'],
+  long_description=open("README.rst").read(),
+  install_requires=open('requirements.txt', 'r').readlines(),
+  classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python',
+    'Operating System :: OS Independent',
+    'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+  ]
+)
